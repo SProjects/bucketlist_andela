eec7833512dc9691c26716219889b6092c800497
Sebuuma Daniel <sedzsoft@gmail.com>
2017-05-22 12:04:21 +0300

    Adds routes to user management endpoints.

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -18,7 +18,13 @@ def create_app(env_name):
     api_v1.add_resource(RegisterUser, '/auth/register')
     api_v1.add_resource(AuthenticateUser, '/auth/login')
 
+    from api.v1.users.views import UserEndpoint, UserList
+    api_v1.add_resource(UserList, '/users')
+    api_v1.add_resource(UserEndpoint, '/users/<int:user_id>')
+
     from api.v1.auth import auth as auth_v1_blueprint
+    from api.v1.users import users as users_v1_blueprint
     app.register_blueprint(auth_v1_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(users_v1_blueprint, url_prefix='/api/v1')
 
     return app
