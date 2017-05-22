c837c86e2f2b7d32e9ca52458a49171d3f6f7dbe
Sebuuma Daniel <sedzsoft@gmail.com>
2017-05-23 01:46:56 +0300

    Adds buckelist endpoint links to create_app.

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -24,9 +24,15 @@ def create_app(env_name):
     api_v1.add_resource(UserList, '/users')
     api_v1.add_resource(UserEndpoint, '/users/<int:user_id>')
 
+    from api.v1.bucketlists.views import BucketListEndpoint, BucketLists
+    api_v1.add_resource(BucketLists, '/bucketlists')
+    api_v1.add_resource(BucketListEndpoint, '/bucketlists/<int:bucketlist_id>')
+
     from api.v1.auth import auth as auth_v1_blueprint
     from api.v1.users import users as users_v1_blueprint
+    from api.v1.bucketlists import bucketlists as bucketlist_v1_blueprint
     app.register_blueprint(auth_v1_blueprint, url_prefix='/api/v1')
     app.register_blueprint(users_v1_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(bucketlist_v1_blueprint, url_prefix='/api/v1')
 
     return app
