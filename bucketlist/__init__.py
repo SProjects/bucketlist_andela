625aef996070b3fb6fd861adbf9a01884b66f575
Sebuuma Daniel <sedzsoft@gmail.com>
2017-05-23 13:45:28 +0300

    Adds items endpoints to create_app()

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -28,11 +28,17 @@ def create_app(env_name):
     api_v1.add_resource(BucketLists, '/bucketlists')
     api_v1.add_resource(BucketListEndpoint, '/bucketlists/<int:bucketlist_id>')
 
+    from api.v1.items.views import ItemEndpoint, ItemsList
+    api_v1.add_resource(ItemsList, '/bucketlists/<int:bucketlist_id>/items')
+    api_v1.add_resource(ItemEndpoint, '/bucketlists/<int:bucketlist_id>/items/<int:item_id>')
+
     from api.v1.auth import auth as auth_v1_blueprint
     from api.v1.users import users as users_v1_blueprint
-    from api.v1.bucketlists import bucketlists as bucketlist_v1_blueprint
+    from api.v1.bucketlists import bucketlists as bucketlists_v1_blueprint
+    from api.v1.items import items as items_v1_blueprint
     app.register_blueprint(auth_v1_blueprint, url_prefix='/api/v1')
     app.register_blueprint(users_v1_blueprint, url_prefix='/api/v1')
-    app.register_blueprint(bucketlist_v1_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(bucketlists_v1_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(items_v1_blueprint, url_prefix='/api/v1')
 
     return app
