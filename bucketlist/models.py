8f70f3e31db76d5727e5b1a4c5856eca36f405cb
Sebuuma Daniel <sedzsoft@gmail.com>
2017-05-23 14:24:43 +0300

    Adds save() and delete() methods to Item. Updates the default value of the Item done field to False.

--- bucketlist/models.py
+++ bucketlist/models.py
@@ -15,10 +15,18 @@ eat_timezone = pytz.timezone('Africa/Nairobi')
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
-    done = db.Column(db.Boolean, default=True)
+    done = db.Column(db.Boolean, default=False)
     modified_at = db.Column(db.DateTime, default=datetime.now(eat_timezone), onupdate=datetime.now(eat_timezone))
     created_at = db.Column(db.DateTime, default=datetime.now(eat_timezone))
     bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'), nullable=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def __repr__(self):
         return '<Item %r>' % self.name
