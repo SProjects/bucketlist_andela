eba232cdaa2dffb10beca41e5017476754c71347
Sebuuma Daniel <sedzsoft@gmail.com>
2017-06-23 10:39:10 +0300

    Adds registration form that allows user to create new accounts.

--- bucketlist/client/src/app/components/user/registration-form/registration-form.component.ts
+++ bucketlist/client/src/app/components/user/registration-form/registration-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../../models/user.model";
+import {UserService} from "../../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registration-form',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
+  message: string = null;
+  model: any = {};
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+  }
+
+  createUser() {
+    this.userService.create(this.model).subscribe(
+      success => {
+        this.message = success;
+        this.router.navigate(['/login']);
+      },
+      error => {
+        this.message = error;
+      }
+    );
   }
 
 }
