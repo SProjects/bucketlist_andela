f3cbde25c957a2774961db075118e8ef4a6d2ece
Sebuuma Daniel <sedzsoft@gmail.com>
2017-07-05 21:34:29 +0300

    Updates implementation of on click actions for the delete feature on bucketlists and bucketlist items.

--- bucketlist/client/static/javascript/app.js
+++ bucketlist/client/static/javascript/app.js
@@ -1,13 +1,9 @@
-$(document).ready(function () {
-    $(".confirm-bucketlist-delete").click(function (event) {
-        event.preventDefault();
-        $("#confirm-delete-modal").modal("show");
-    });
+$(document).on("click", ".confirm-bucketlist-delete", function (event) {
+  event.preventDefault();
+  $("#confirm-delete-modal").modal("show");
 });
 
-$(document).ready(function () {
-   $(".confirm-item-delete").click(function (event) {
-      event.preventDefault();
-      $("#confirm-delete-item-modal").modal("show");
-   });
+$(document).on("click", ".confirm-item-delete", function (event) {
+  event.preventDefault();
+  $("#confirm-delete-item-modal").modal("show");
 });
