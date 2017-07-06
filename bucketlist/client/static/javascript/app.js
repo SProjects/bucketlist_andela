1cb7dd4f4c982741d42d9a4d75eb730acf2785da
Sebuuma Daniel <sedzsoft@gmail.com>
2017-07-06 12:59:26 +0300

    Adds jquery to remove error messages on the login page.

--- bucketlist/client/static/javascript/app.js
+++ bucketlist/client/static/javascript/app.js
@@ -7,3 +7,7 @@ $(document).on("click", ".confirm-item-delete", function (event) {
   event.preventDefault();
   $("#confirm-delete-item-modal").modal("show");
 });
+
+$(document).on('click', ".message .close", function() {
+    $(this).closest('.message').transition('fade');
+});
